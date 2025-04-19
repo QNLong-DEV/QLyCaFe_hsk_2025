a6bd8617a0b36591ff4450a0627d7c1fbaa4e4fa
Martus <dechebacninh2@gmail.com>
2025-04-19 22:40:36 +0700

    up lib

--- src/main/mainTest.java
+++ src/main/mainTest.java
@@ -2,6 +2,6 @@ package main;
 
 public class mainTest {
 	public static void main(String[] args) {
-		System.out.println(123);
+	
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module QLyCaFe_hsk_2025 {
+	requires java.sql;
 }
