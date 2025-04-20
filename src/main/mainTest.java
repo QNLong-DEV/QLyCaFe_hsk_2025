4c30b86f46c967ea0091ecdc3f945c4a660dca08
Martus <dechebacninh2@gmail.com>
2025-04-20 14:23:06 +0700

    update DAO,demo getALL

--- src/main/mainTest.java
+++ src/main/mainTest.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import connection.connectiondb;
+import controller.DanhSachNhanVien;
+import dao.NhanVienDAO;
 import gui.MenuGUI;
 import gui.crudNhanVien;
+import model.NhanVien;
 
 public class mainTest {
+	private static DanhSachNhanVien listNV;
+
 	public static void main(String[] args) {
 		new MenuGUI();
+		NhanVienDAO dao = new NhanVienDAO();
+		listNV = new DanhSachNhanVien();
+		listNV = dao.layDanhSachNhanVien();
+		System.out.println(listNV);
 	}
 }
