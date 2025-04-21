8340a0574d7e1e5c0fe3f62ac33c71f5d79037db
Martus <dechebacninh2@gmail.com>
2025-04-22 00:07:39 +0700

    tao ham createCard, class nuocDAO,dsNuoc,Nuoc, getAllmenuNuoc

---
+++ src/controller/DanhSachNuoc.java
@@ -0,0 +1,76 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.NhanVien;
+import model.Nuoc;
+
+public class DanhSachNuoc {
+	private ArrayList<Nuoc> list;
+
+	public DanhSachNuoc() {
+		list = new ArrayList<Nuoc>();
+	}
+
+	public ArrayList<Nuoc> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<Nuoc> list) {
+		this.list = list;
+	}
+
+	public boolean addList(Nuoc nuoc) {
+		for (Nuoc x : list) {
+			if (x.getMaNuoc().equalsIgnoreCase(nuoc.getMaNuoc())) {
+				return false;
+			}
+		}
+		return list.add(nuoc);
+	}
+
+	public boolean deleteList(String manuoc) {
+		for (Nuoc x : list) {
+			if (x.getMaNuoc().equalsIgnoreCase(manuoc)) {
+				list.remove(x);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean updateList(Nuoc nuoc) {
+		int index = list.indexOf(nuoc);
+		for (Nuoc x : list) {
+			if (x.getMaNuoc().equalsIgnoreCase(nuoc.getMaNuoc())) {
+				list.set(index, nuoc);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int getSize() {
+		return list.size();
+	}
+
+	public boolean checktrungma(String manuoc) {
+		for (Nuoc x : list) {
+			if (x.getMaNuoc().equalsIgnoreCase(manuoc)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean removeNV(String manuoc) {
+		for (Nuoc x : list) {
+			if (x.getMaNuoc().equalsIgnoreCase(manuoc)) {
+				list.remove(x);
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
